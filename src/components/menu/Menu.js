549248ca77126cef73876236403fd75d9a2829f7
David Savenmark <dsinemark@gmail.com>
2021-04-26 17:45:35 +0200

    added styles for menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,18 +4,19 @@ import './menu.css';
 const Menu =()=>{
     return(
         <div>
-            <div className="mobile-menu">
+            <div className="burger">
                 <div className="lines">
-                <div className="line">
-                <div className="line">
-                <div className="line">
-
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
                 </div>
             </div>
             <div className="menu-container">
-                <div className="menu-logo"></div>
+                <div className="menu-logo"><img className="menu-logo-image"src={require('../../assets/logo.png')}alt="dog"/></div>
                 <div className="menu-title"></div>
             </div>
         </div>
     );
 }
+
+export default Menu;
